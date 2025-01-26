f1937c63134635f75fd749afb3cefacd0eff43d4
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2025-01-26 18:15:57 -0500

    changed spinner color

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -4,7 +4,11 @@ import "bootstrap/dist/css/bootstrap.css"
 const Spinner = () => {
   return (
     <div className="text-center">
-      <div className="spinner-border text-primary" role="status">
+      <div 
+        className="spinner-border text-primary" 
+        role="status"
+        style={{ color: "#036042" }}
+      >
         <span className="sr-only"></span>
       </div>
     </div>
